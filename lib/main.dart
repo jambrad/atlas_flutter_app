75fbbae90bf2c9e64240af91d5fcca58f7eb5492
Jhon Christian Ambrad <jambrad123@gmail.com>
2021-08-12 01:47:55 +0800

    Integrate aws amplify

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'dart:typed_data';
+
 import 'package:atlas_flutter_app/sensor_data_widget.dart';
 import 'package:flutter/material.dart';
-
+import 'package:amplify_flutter/amplify.dart';
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'amplifyconfiguration.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -31,6 +36,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  void initState() {
+    _configureAmplify();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -41,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-                onPressed: () => _navigateToSensorDataWidget("userAccelerometer"),
+                onPressed: () => onTestApi(), //_navigateToSensorDataWidget("userAccelerometer"),
                 child: const Text("See accelerometer data")),
             ElevatedButton(
                 onPressed: () => _navigateToSensorDataWidget("gyroscope"),
@@ -50,6 +61,41 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+
+  void _configureAmplify() async {
+    // Add the following line to add API plugin to your app.
+    // Auth plugin needed for IAM authorization mode, which is default for REST API.
+    Amplify.addPlugins([AmplifyAPI(), AmplifyAuthCognito()]);
+
+    try {
+      await Amplify.configure(amplifyconfig);
+    } on AmplifyAlreadyConfiguredException {
+      print(
+          "Tried to reconfigure Amplify; this can occur when your app restarts on Android.");
+    }
+  }
+
+  void onTestApi() async {
+    if (Amplify.isConfigured) {
+      try {
+        RestOptions options = RestOptions(
+            path: '/api',
+            body: Uint8List.fromList('{\'name\':\'Mow the lawn\'}'.codeUnits)
+        );
+        RestOperation restOperation = Amplify.API.post(
+            restOptions: options
+        );
+        RestResponse response = await restOperation.response;
+        print('POST call succeeded');
+        print(String.fromCharCodes(response.data));
+      } on ApiException catch (e) {
+        print('POST call failed: $e');
+      }
+    } else {
+      print('Amplify not configured!');
+    }
+
   }
 
   _navigateToSensorDataWidget(String type) {
